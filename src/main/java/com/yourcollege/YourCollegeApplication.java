a23c74375d7553cd2cb66ccd9376231e62c8e4a3
ankit.jain <ankit.jain@dream11.com>
2020-06-07 17:57:03 +0530

    Hello world

---
+++ src/main/java/com/yourcollege/YourCollegeApplication.java
@@ -0,0 +1,13 @@
+package com.yourcollege;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourCollegeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YourCollegeApplication.class, args);
+	}
+
+}
